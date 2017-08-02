77773204533bfeb70d9c86ef7006ad5ff19c0c9f
monkeyfont <juddkw@gmail.com>
2017-08-02 18:52:21 +1200

    sprites are cliable and logged to console. map was changed to be 1080p

--- Animation Front End/spriteAnimate.js
+++ Animation Front End/spriteAnimate.js
@@ -1,7 +1,27 @@
+var snd = new Audio("A Instrumental Masterpiece.mp3"); // buffers automatically when created
+snd.play();
+
+$('.Audio').on('click', function(e){
+		alert("Button clicked with value: "+e.currentTarget.value);
+		if (e.currentTarget.id == "Play"){
+			alert("play");
+		}else if(e.currentTarget.id == "Mute"){
+			alert("Mute");
+		}
+		
+	});
+	
+	
+
+	
 var coinImage = new Image();
 coinImage.src = "coin-sprite-animation.png";
+var playerImage = new Image();
+playerImage.src = 'player6.png';
 var mapImage = new Image();
 mapImage.src = 'WorldMap.jpg';
+var cityImage = new Image();
+cityImage.src = 'city token.png';
 var scrnWidth = window.innerWidth;//screen.width;
 var scrnHeight = window.innerHeight;//screen.height;
 console.log("Total Width: "+scrnWidth+" Total Height: "+scrnHeight)
@@ -13,6 +33,10 @@ var screenWidthPercentage = scrnWidth/optimalScreenWidth;
 
 var scaleSize = 1;
 
+var cities = {city1:{x:100,y:100},city2:{x:400,y:400},city3:{x:630,y:720}};
+
+
+
 // sets the scalesize to the lower of height or width
 if (screenHeightPercentage<screenWidthPercentage){
 	scaleSize = screenHeightPercentage;
@@ -21,23 +45,57 @@ if (screenHeightPercentage<screenWidthPercentage){
 }
 
 var canvas = document.getElementById("myCanvas");
+canvas.width = 1920*scaleSize;
+canvas.height = 1080*scaleSize;
 canvas.getContext("2d").scale(scaleSize,scaleSize);
+
+// console.log(canvas.getContext("2d").getScale())
 console.log("Total Width Percent: "+screenWidthPercentage+" Total Height Percent: "+screenHeightPercentage);
 console.log("scale Size:"+scaleSize);
+
+
+
+
+canvas.addEventListener('click', function(evt) {
+	var mousePos ={
+		x: (evt.clientX - canvas.getBoundingClientRect().left)/scaleSize,
+		y: (evt.clientY - canvas.getBoundingClientRect().top)/scaleSize
+	}	
+	var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+	console.log(message);
+	for (var i in spriteList){
+		// is the click on the image?
+		if (mousePos.x >= spriteList[i].xPos && mousePos.x <= spriteList[i].xPos+(spriteList[i].width*spriteList[i].xScale) &&
+			mousePos.y >= spriteList[i].yPos && mousePos.y <= spriteList[i].yPos+(spriteList[i].height*spriteList[i].yScale)){
+				console.log(spriteList[i].id,"was clicked");
+			}
+		
+		
+		
+	}
+})
+
+
 
 function gameLoop(){
 	window.requestAnimationFrame(gameLoop);
 	// map.update();
 	// map.render();
 	canvas.getContext("2d").drawImage(mapImage,0,0)// quick workaround because loading the map as a sprite is broken
-	console.log("coin3"+coin3.tickCount);
-	console.log("coin2"+coin2.tickCount);
+	
+	for (var i in cities){
+		canvas.getContext("2d").drawImage(cityImage,cities[i].x,cities[i].y);
+	}
+	
 	coin.update();
 	coin.render();
 	coin2.update();
 	coin2.render();
 	coin3.update();
 	coin3.render();
+	player.update();
+	player.render();
+	
 
 	//console.log("gameloop");
 }
@@ -45,7 +103,7 @@ function gameLoop(){
 
 
 function sprite(options) {
-				
+	this.id = options.id,			
 	this.frameIndex = 0, //current frame being rendered
 	this.tickCount = 0,	// number of updates since the last render 
 	this.ticksPerFrame = options.ticksPerFrame || 0; //by having ticks per frame, it allows us to slow the animation down, and still fun a game at 60fps, 
@@ -57,6 +115,8 @@ function sprite(options) {
 	this.yPos = options.yPos || 0;
 	this.xPos = options.yPos || 0;
 	this.numberOfFrames = options.numberOfFrames || 1;
+	this.xScale = options.xScale || 1;
+	this.yScale = options.yScale || 1;
 	
 	this.update = function(){ //update the frame every x ticks 
 		//console.log("updated",this.image.src);
@@ -82,8 +142,8 @@ function sprite(options) {
 		this.height, // height of clipped image
 		this.xPos, //x position for image on canvas
 		this.yPos, // y position for image on canvas
-		this.width, // width of image to use 
-		this.height); // height of image to use
+		this.width*this.xScale, // width of image to use 
+		this.height*this.yScale); // height of image to use
     };
 }
 
@@ -103,6 +163,7 @@ function sprite(options) {
 	// });	
 	
 var coin3 = new sprite({
+	id:"coin3",
     context: canvas.getContext("2d"),
     width: 100,
     height: 100,
@@ -114,6 +175,7 @@ var coin3 = new sprite({
 	});	
 
 var coin = new sprite({
+	id:"coin",
     context: canvas.getContext("2d"),
     width: 100,
     height: 100,
@@ -125,6 +187,7 @@ var coin = new sprite({
 	});		
 	
 var coin2 = new sprite({
+	id:"coin2",
     context: canvas.getContext("2d"),
     width: 100,
     height: 100,
@@ -134,6 +197,23 @@ var coin2 = new sprite({
 	yPos:400,
     image: coinImage	
 	});	
+	
+var player = new sprite({
+	id:"player",
+	context: canvas.getContext("2d"),
+    width: 32,
+    height: 40,
+	numberOfFrames: 4,
+	ticksPerFrame: 16,
+	xPos:600,
+	yPos:600,
+	xScale:2,
+	yScale:2,
+    image: playerImage
+	
+})
+
+spriteList = [coin,coin2,coin3,player];
 	
 mapImage.addEventListener("load", gameLoop);	
 // coinImage.addEventListener("load", gameLoop);
