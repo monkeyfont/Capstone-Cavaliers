075f64ab1c701778e2c3758fd99d4655cdd0f1de
monkeyfont <juddkw@gmail.com>
2017-08-02 23:59:57 +1200

    audio controls working to mute/unmute play/pause

--- Animation Front End/spriteAnimate.js
+++ Animation Front End/spriteAnimate.js
@@ -2,10 +2,21 @@ var snd = new Audio("A Instrumental Masterpiece.mp3"); // buffers automatically 
 snd.play();
 
 $('.Audio').on('click', function(e){
-		alert("Button clicked with value: "+e.currentTarget.value);
+		// alert("Button clicked with value: "+e.currentTarget.value);
 		if (e.currentTarget.id == "Play"){
+			if (snd.paused){
+				snd.play();
+			}else{
+				snd.pause();
+			}
+			
 			alert("play");
 		}else if(e.currentTarget.id == "Mute"){
+			if (snd.muted == true){
+				snd.muted = false;
+			}else{
+				snd.muted = true;
+			}
 			alert("Mute");
 		}
 		
