4c8fa474d2adda86e2be6d85e2dfbfdb06c06dc9
monkeyfont <juddkw@gmail.com>
2017-08-08 21:28:40 +1200

    move character v2

--- server_files/static/js/spriteAnimate.js
+++ server_files/static/js/spriteAnimate.js
@@ -273,29 +273,54 @@ function sprite(options) {
 	this.moveX = this.xPos;
 	this.moveY = this.yPos;
 	this.speed = options.speed || 10;
+	this.tempSpeed = 0;
+	
 	
 	this.move = function (x, y){
 		this.moveX = x;
 		this.moveY = y;		
 	}
 	
-	this.update = function(){ 
-		if (this.moveX != this.xPos){
-			if (this.moveX <= this.xPos){
-				this.xPos = this.xPos - this.speed
+	
+	this.update = function(){		
+		
+		var deltaX = this.xPos - this.moveX;
+		var deltaY = this.yPos - this.moveY;
+		if ( deltaX != 0  && deltaY != 0){
+			// work out the distance a^2 + b^2 = c^2 where deltaX and DeltaY are a and b
+			
+			var distance = Math.sqrt((deltaX**2)+(deltaY**2));
+			console.log("distance",distance);
+			this.tempSpeed = distance / Math.ceil(distance / this.speed);
+			console.log("temp speed",this.tempSpeed)
+			var incrementX = deltaX/(distance/this.tempSpeed);
+			var incrementY = deltaY/(distance/this.tempSpeed);
+			console.log("increment",incrementX,incrementY);
+			
+			if (this.moveX == this.xPos){
+				this.tempSpeed = 10;
+			}else if (this.moveY == this.yPos){
+				this.tempSpeed = 10;
 			}else{
-				this.xPos = this.xPos + this.speed
-			}
-		}
-		if (this.moveY != this.yPos){
-			if (this.moveY <= this.yPos){
-				this.yPos = this.yPos - this.speed
-			}else{
-				this.yPos = this.yPos + this.speed
+				
 			}
 			
+			if (this.moveX != this.xPos){
+				if (this.moveX <= this.xPos){
+					this.xPos = this.xPos - incrementX
+				}else{
+					this.xPos = this.xPos - incrementX
+				}
+			}
+			if (this.moveY != this.yPos){
+				if (this.moveY <= this.yPos){
+					this.yPos = this.yPos - incrementY
+				}else{
+					this.yPos = this.yPos - incrementY
+				}
+				
+			}
 		}
-	
 		//update the frame every x ticks 
 		//console.log("updated",this.image.src);
 		this.tickCount +=1;
