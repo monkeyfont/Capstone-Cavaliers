9570505426508700c846d567cae904146494fac4
JorgeST <jsolistr@gmail.com>
2017-08-14 11:31:05 +1200

    Rooms with Objects
    Now users can join different rooms.
    Users can create their own rooms.

--- server_files/__init__.py
+++ server_files/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, make_response
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
+
 from game import *
 import random
 import unittest
@@ -10,11 +11,10 @@ socketio = SocketIO(app)
 room_ID = 1
 playerID = 1
 games = {} # in here we will store the game objects
+userTable = {} # this will be changed later on to be a database storing the user details
 @app.route('/')
 def home():
     # Quick session testing code.
-    session["username"] = "Player " + str(random.randrange(1000))
-    print(session["username"])
     return render_template("home.html")
 
 @app.route('/game')
@@ -22,14 +22,12 @@ def game():
     session["username"] = "Player " + str(random.randrange(1000))
     return render_template("MapOnCanvas.html")
 
-
 @socketio.on('join')
 def joined(msg):
     room = "1" # room = session.get('room')
     player = session["username"]
     join_room(room)
-    emit('joined', {'msg' : str(player + " joined room" + room)}, room=room)
-
+    emit('joined', {'msg' : "Player " + str(player) + " joined room " + room}, room=room)
 
 
 @socketio.on('joinGame')
@@ -37,7 +35,7 @@ def joined(msg):
     room = "1" # room = session.get('room')
     player = session["username"]
     join_room(room)
-    emit('joined', {'msg' : str(player + " joined room" + room)}, room=room)
+    emit('joined', {'msg' : "Player " + str(player + " joined room " + room)}, room=room)
 
 
 
@@ -46,9 +44,7 @@ def handleMessage(msg):
     room = "1"
     player = session["username"]
     location = msg["move_location"]
-    emit('moved', {'msg' : player + " moved to " + location}, room=room)
-
-
+    emit('moved', {'msg' : str(player) + " moved to " + location}, room=room)
 
 @socketio.on('click')
 def handleclick(msg):
@@ -79,11 +75,12 @@ def handle_message(msg):
 
 
 ##############      PAGES WORKING ON       #####################################
-# in this page users can create a new room or join an existing room
+
 @app.route('/room')
 def room():
     session["username"] = "Player " + str(random.randrange(1000))
     return render_template("room.html")
+
 
 # this is the page were users go to after they have decided to either start a game or join a game
 @app.route('/user')
@@ -92,48 +89,54 @@ def user():
     return render_template("userpage.html")
 ####################################################################################
 ##############      fUNCTIONS WORKING ON       #####################################
+
 @socketio.on('newroom')
 def handleMessage(msg):
     global playerID
     global room_ID
-    new_game_id = room_ID
-    player_name = msg["playerName"]
 
+   # sessionDetails = [x.strip() for x in session["username"].split(' ')]
+
+    room = str(room_ID)
+    session["username"] = msg["playerName"]
+    player = session["username"]
     gameobject = GameBoard()
-    gameobject.gameID = new_game_id
+    gameobject.gameID = room
 
-    gamePlayer = Player(playerID,player_name)
+    gamePlayer = Player(playerID,player)
     gameobject.playerCount = 1
     gameobject.players[gameobject.playerCount] = gamePlayer
 
-    games[new_game_id] = gameobject
+    games[room] = gameobject
     playerID = playerID + 1
 
+    join_room(room)
+    emit('created', {'msg' : "Player " + str(player) + " created room " + room}, room=room)
+    room_ID = room_ID + 1
 
-    join_room(new_game_id)
-    room_ID = new_game_id + 1
+
 
 
 @socketio.on('joinexistingroom')
 def handleMessage(msg):
     global playerID
-    new_room_id = int(msg["roomName"])
+    room = msg["roomName"]
     #get the gameboard called
-    gameCalled = games[new_room_id]
+    gameCalled = games[room]
     numberOfPlayers = gameCalled.playerCount
 
     if numberOfPlayers == 4:
         print "Too many players"
     else:
-        player_name = msg["playerName"]
-        player = Player(playerID,player_name)
+        session["username"] = msg["playerName"]
+        player = session["username"]
+        player = Player(playerID,player)
+        numberOfPlayers = gameCalled.playerCount + 1
 
-        gameobject = games[new_room_id]
-        numberOfPlayers = gameobject.playerCount + 1
-
-        gameobject.players[numberOfPlayers] = player
-        gameobject.playerCount = gameobject.playerCount + 1
-        join_room(new_room_id)
+        gameCalled.players[numberOfPlayers] = player
+        gameCalled.playerCount = gameCalled.playerCount + 1
+        join_room(room)
+        emit('joined', {'msg' : "Player " + str(session["username"]) + " joined room " + str(room)}, room=room)
 
 
 
--- server_files/static/js/roomChoice.js
+++ server_files/static/js/roomChoice.js
@@ -5,10 +5,22 @@ $(document).ready(function () {
 
 
 
-    $("#create_room_button").click(function(){
+ $("#userLogin").click(function(){
+
+        var userName = $('#userName').val();
+        var password = $('#userPassword').val();
+        // clear the field
+        $('#userName').val("");
+        $('#userPassword').val("");
+        // submit that value
+        socket.emit('loguser', {userN:userName,userP:password})
+
+        // submit request for new room
+    });
+
+ $("#create_room_button").click(function(){
 
         // get the value from the join room box
-        var value = $('#new_room_id').val();
         var name = $('#new_user_name').val();
         // clear the field
         $('#new_user_name').val("");
@@ -16,15 +28,6 @@ $(document).ready(function () {
         socket.emit('newroom', {playerName:name})
 
         // submit request for new room
-
-
-        var value = $('#new_room_id').val();
-        // clear the field
-        $('#new_room_id').val("");
-        // submit that value
-        socket.emit('newroom', {roomName:value})
-
-        location.href = "user"
 
     });
 
@@ -37,7 +40,31 @@ $(document).ready(function () {
         $('#user_name').val("");
         // submit that value
         socket.emit('joinexistingroom', {roomName:value,playerName:name})
-        location.href = "user"
+    });
+
+
+
+
+    socket.on('joined', function (data) {
+        $('#log').val($('#log').val() + data.msg + '\n');
+    });
+
+    $("#movement_button").click(function(){
+        // get the value from the movement box
+        var value = $('#movement_input').val();
+        // clear the field
+        $('#movement_input').val("");
+        // submit that value
+        socket.emit('move', {move_location:value})
+
+    });
+
+    socket.on('moved', function (data) {
+        $('#log').val($('#log').val() + data.msg + '\n');
+    });
+
+    socket.on('created', function (data) {
+        $('#log').val($('#log').val() + data.msg + '\n');
     });
 
 });
