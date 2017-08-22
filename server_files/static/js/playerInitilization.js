9cfb6ab9c6a464688f34672b75d61fb2151becf3
monkeyfont <juddkw@gmail.com>
2017-08-22 17:42:59 +1200

    PlayerInitilizationFunction Started

---
+++ server_files/static/js/playerInitilization.js
@@ -0,0 +1,15 @@
+var contingencyPlanner = new Image(); contingencyPlanner.scr = 'static/images/PlayerIcons/contingencyPlanner.png';
+var dispatcher = new Image(); dispatcher.scr = 'static/images/PlayerIcons/dispatcher.png';
+var medic = new Image(); medic.scr = 'static/images/PlayerIcons/medic.png';
+var operationsExpert = new Image(); operationsExpert.scr = 'static/images/PlayerIcons/operationsExpert.png';
+var quarantineSpecialist = new Image(); quarantineSpecialist.scr = 'static/images/PlayerIcons/quarantineSpecialist.png';
+var researcher = new Image(); researcher.scr = 'static/images/PlayerIcons/researcher.png';
+var scientist = new Image(); scientist.scr = 'static/images/PlayerIcons/scientist.png';
+
+
+function playerInitilization (options){
+	
+	
+	
+	
+}
