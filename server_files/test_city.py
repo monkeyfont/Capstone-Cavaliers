c34fdef37f159d3680c644c913b85ae2e83b18c0
Clazzasaur <clazzasaur@users.noreply.github.com>
2017-08-29 16:32:09 +1200

    Added blank tests.

---
+++ server_files/test_city.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+
+class TestCity(TestCase):
+    def test_addResearchStation(self):
+        self.fail()
+
+    def test_removeResearchStation(self):
+        self.fail()
+
+    def test_getResearchStation(self):
+        self.fail()
+
+    def test_getConnections(self):
+        self.fail()
+
+    def test_getInfections(self):
+        self.fail()
+
+    def test_infect(self):
+        self.fail()
+
+    def test_treat(self):
+        self.fail()
---
+++ server_files/test_gameBoard.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+
+
+class TestGameBoard(TestCase):
+    def test_endTurnDrawCards(self):
+        self.fail()
+
+    def test_endTurnInfectCities(self):
+        self.fail()
+
+    def test_resetPlayerActions(self):
+        self.fail()
+
+    def test_totalPlayerActions(self):
+        self.fail()
+
+    def test_movePlayer(self):
+        self.fail()
+
+    def test_directFlight(self):
+        self.fail()
+
+    def test_charterFlight(self):
+        self.fail()
+
+    def test_shuttleFlight(self):
+        self.fail()
+
+    def test_buildResearchStation(self):
+        self.fail()
+
+    def test_shareKnowledgeTake(self):
+        self.fail()
+
+    def test_shareKnowledgeGive(self):
+        self.fail()
+
+    def test_discoverCure(self):
+        self.fail()
+
+    def test_treatDisease(self):
+        self.fail()
+
+    def test_infectCity(self):
+        self.fail()
