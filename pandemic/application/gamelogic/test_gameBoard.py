8a39f50b3dbfb5b664e4816a2b67225231086342
cliff <clazzasaur@gmail.com>
2017-10-19 01:19:39 +1300

    Test discards, several test outBreak functions (including chain outbreaks)

--- pandemic/application/gamelogic/test_gameBoard.py
+++ pandemic/application/gamelogic/test_gameBoard.py
@@ -307,10 +307,39 @@ class TestGameActions(TestCase):
         self.assertEquals(result["validAction"], True)
 
     def test_playerDiscard(self):
-        pass
+        # add Atlanta to player 1 hand
+        # remove atlanta with discardCard()
+        # result should be true.
+        # card should not be in their hand.
+        card = PlayerCard("ATLANTA", "blue", "", "", "")
+        player1 = self.testGameBoard.players[1]
+        player1.hand.append(card)
+        result = self.testGameBoard.discardCard(1, "ATLANTA")
+        self.assertTrue(result)
+        self.assertFalse(card in player1.hand)
 
-    def test_playerDiscardFail(self):
-        pass
+    def test_playerDiscard2(self):
+        # add several cards, including MIAMI to player 1 hand
+        # remove atlanta with discardCard()
+        # result should be true.
+        # card should not be in their hand.
+        card1 = PlayerCard("ATLANTA", "blue", "", "", "")
+        card2 = PlayerCard("LONDON", "blue", "", "", "")
+        card3 = PlayerCard("SYDNEY", "red", "", "", "")
+        card4 = PlayerCard("MIAMI", "yellow", "", "", "")
+        player1 = self.testGameBoard.players[1]
+        player1.hand += [card1, card2, card3, card4]
+        result = self.testGameBoard.discardCard(1, "MIAMI")
+        self.assertTrue(result)
+        self.assertFalse(card4 in player1.hand)
+
+    def test_playerDiscardFails1(self):
+        # removing a card from a players hand should be false if they don't have it.
+        card = PlayerCard("ATLANTA", "blue", "", "", "")
+        player1 = self.testGameBoard.players[1]
+        player1.hand.append(card)
+        result = self.testGameBoard.discardCard(1, "LONDON")
+        self.assertFalse(result)
 
 
 
@@ -320,8 +349,200 @@ class TestGameCoordinator(TestCase):
         d = {1: Player(1, "p1"), 2: Player(2, "p2"), 3: Player(3, "p3"), 4: Player(4, "p4")}
         self.testGameBoard = GameBoard(d, initialize = False)
 
-    def test_outBreak(self):
-        pass
+
+    def test_forcedOutBreak(self):
+        """
+        This test will check that the cityOutBreak() internal function works correctly.
+        (In the real game, this will not be called directly, but will as a subcall from infectCity() )
+        When infected, if a city has 3 infections it will spread to connected neighbouring cities rather than itself.
+        """
+        # infect atlanta with 3 blues.
+        # call cityOutBreak function on it ( requires obj and colour)
+        # washington, miami, chicago should all be infected with ONE blue each. Atlanta should remain at 3.
+        self.testGameBoard.cities = self.testGameBoard.generateCities()
+        atlanta = self.testGameBoard.cities["ATLANTA"]
+        miami = self.testGameBoard.cities["MIAMI"]
+        chicago = self.testGameBoard.cities["CHICAGO"]
+        washington = self.testGameBoard.cities["WASHINGTON"]
+        atlanta.blue = 3
+        self.testGameBoard.cityOutBreak(atlanta, 'blue')
+        self.assertEquals(chicago.blue, 1)
+        self.assertEquals(washington.blue, 1)
+        self.assertEquals(atlanta.blue, 3)
+        self.assertEquals(miami.blue, 1)
+
+    def test_forcedOutBreakSafetyCheck(self):
+        """
+        A safecheck. If the city DOESNT actually have 3 infections on it, it should just do a normal infection.
+        """
+        # infect atlanta with !! 2 !! blues.
+        # call cityOutBreak function on it ( requires obj and colour)
+        # all connected cities should remain at zero infections. Atlanta should gain one infection.
+        self.testGameBoard.cities = self.testGameBoard.generateCities()
+        atlanta = self.testGameBoard.cities["ATLANTA"]
+        miami = self.testGameBoard.cities["MIAMI"]
+        chicago = self.testGameBoard.cities["CHICAGO"]
+        washington = self.testGameBoard.cities["WASHINGTON"]
+        atlanta.blue = 2
+        self.testGameBoard.cityOutBreak(atlanta, 'blue')
+        self.assertEquals(chicago.blue, 0)
+        self.assertEquals(washington.blue, 0)
+        self.assertEquals(atlanta.blue, 3)
+        self.assertEquals(miami.blue, 0)
+
+    def test_StandardOutBreak(self):
+        """
+        This test actually tests the process where the game will cause an outbreak from the coordinator.
+        It uses the infectCity() function, which should cause an outbreak if a city has 3 infections on it.
+        """
+        # infect atlanta with 3 blues.
+        # call infectCity function on it ( requires obj and colour)
+        # washington, miami, chicago should all be infected with ONE blue each. Atlanta should remain at 3.
+        self.testGameBoard.cities = self.testGameBoard.generateCities()
+        atlanta = self.testGameBoard.cities["ATLANTA"]
+        miami = self.testGameBoard.cities["MIAMI"]
+        chicago = self.testGameBoard.cities["CHICAGO"]
+        washington = self.testGameBoard.cities["WASHINGTON"]
+        atlanta.blue = 3
+        self.testGameBoard.infectCity("ATLANTA", 'blue')
+        self.assertEquals(chicago.blue, 1)
+        self.assertEquals(washington.blue, 1)
+        self.assertEquals(miami.blue, 1)
+        self.assertEquals(atlanta.blue, 3)
+
+    def test_StandardOutBreak2(self):
+        """
+        Tests a different part of the map, with red infections.
+        """
+        # infect sydney with 3 reds
+        # call infectCity function on it ( requires obj and colour)
+        # jakarta, manila, losangeles should be infected with 1 red token each. Sydney should remain at 3.
+        self.testGameBoard.cities = self.testGameBoard.generateCities()
+        sydney = self.testGameBoard.cities["SYDNEY"]
+        jakarta = self.testGameBoard.cities["JAKARTA"]
+        manila = self.testGameBoard.cities["MANILA"]
+        losangeles = self.testGameBoard.cities["LOSANGELES"]
+        sydney.red = 3
+        self.testGameBoard.infectCity("SYDNEY", amount=1)
+        self.assertEquals(jakarta.red, 1)
+        self.assertEquals(manila.red, 1)
+        self.assertEquals(losangeles.red, 1)
+        self.assertEquals(sydney.red, 3)
+
+    def test_StandardOutBreak3(self):
+        """
+        Tests a different part of the map, with black infections.
+        """
+        # infect BAGHDAD with 3 blacks.
+        # ISTANBUL, CAIRO, RIYADH, KARACHI, TEHRAN should be infected with 1 black token each. BAGHDAD should remain at 3.
+        self.testGameBoard.cities = self.testGameBoard.generateCities()
+        baghdad = self.testGameBoard.cities["BAGHDAD"]
+        istanbul = self.testGameBoard.cities["ISTANBUL"]
+        cairo = self.testGameBoard.cities["CAIRO"]
+        riyadh = self.testGameBoard.cities["RIYADH"]
+        karachi = self.testGameBoard.cities["KARACHI"]
+        tehran = self.testGameBoard.cities["TEHRAN"]
+        baghdad.black = 3
+        self.testGameBoard.infectCity("BAGHDAD", amount=1)
+        self.assertEquals(istanbul.black, 1)
+        self.assertEquals(cairo.black, 1)
+        self.assertEquals(riyadh.black, 1)
+        self.assertEquals(karachi.black, 1)
+        self.assertEquals(tehran.black, 1)
+        self.assertEquals(baghdad.black, 3)
+
+    def test_StandardOutBreak4(self):
+        """
+        Infects the black city with RED tokens. the connected cities Should NOT be infected with either black or red tokens.
+        baghdad will remain at 3 reds, but go from 0 to 1 black. All connected cities should not have red OR blacks as no outbreak occured.
+        """
+        self.testGameBoard.cities = self.testGameBoard.generateCities()
+        baghdad = self.testGameBoard.cities["BAGHDAD"]
+        istanbul = self.testGameBoard.cities["ISTANBUL"]
+        cairo = self.testGameBoard.cities["CAIRO"]
+        riyadh = self.testGameBoard.cities["RIYADH"]
+        karachi = self.testGameBoard.cities["KARACHI"]
+        tehran = self.testGameBoard.cities["TEHRAN"]
+        baghdad.red = 3
+        self.testGameBoard.infectCity("BAGHDAD", amount=1)
+        self.assertEquals(istanbul.black, 0)
+        self.assertEquals(cairo.black, 0)
+        self.assertEquals(riyadh.black, 0)
+        self.assertEquals(karachi.black, 0)
+        self.assertEquals(tehran.black, 0)
+        self.assertEquals(istanbul.red, 0)
+        self.assertEquals(cairo.red, 0)
+        self.assertEquals(riyadh.red, 0)
+        self.assertEquals(karachi.red, 0)
+        self.assertEquals(tehran.red, 0)
+        self.assertEquals(baghdad.red, 3)
+        self.assertEquals(baghdad.black, 1)
+
+
+    def test_2xchainOutBreak(self):
+        """
+        Chain outbreaks occur if a outbreak in a city causes another outbreak to occur (both cities get infected and both have
+        3 tokens on them of the same colour)
+        """
+        # infect atlanta and washington with 3 tokens.
+        # call infectCity on atlanta. both atlanta and washington should outbreak.
+        # chicago, montreal, newyork should have 1 blue token each.
+        # miami should have 2 blue tokens (one from each outbreak!)
+
+        self.testGameBoard.cities = self.testGameBoard.generateCities()
+        atlanta = self.testGameBoard.cities["ATLANTA"]
+        washington = self.testGameBoard.cities["WASHINGTON"]
+        miami = self.testGameBoard.cities["MIAMI"]
+        chicago = self.testGameBoard.cities["CHICAGO"]
+        newyork = self.testGameBoard.cities["NEWYORK"]
+        montreal = self.testGameBoard.cities["MONTREAL"]
+
+        atlanta.blue = 3
+        washington.blue = 3
+        self.testGameBoard.infectCity("ATLANTA", 'blue')
+        self.assertEquals(chicago.blue, 1)
+        self.assertEquals(newyork.blue, 1)
+        self.assertEquals(montreal.blue, 1)
+        self.assertEquals(miami.blue, 2)
+        self.assertEquals(atlanta.blue, 3)
+        self.assertEquals(washington.blue, 3)
+
+    def test_3xchainOutBreak(self):
+        """
+        Chain outbreaks occur if a outbreak in a city causes another outbreak to occur (both cities get infected and both have
+        3 tokens on them of the same colour)
+        """
+        # infect atlanta,washington,new york with 3 tokens.
+        # call infectCity on atlanta. all 3 cities should outbreak.
+        # 3 tokens: new york, washington, atlanta
+        # 2 tokens: montreal, miami
+        # 1 token: chicago, madrid, london
+
+        self.testGameBoard.cities = self.testGameBoard.generateCities()
+        atlanta = self.testGameBoard.cities["ATLANTA"]
+        washington = self.testGameBoard.cities["WASHINGTON"]
+        miami = self.testGameBoard.cities["MIAMI"]
+        newyork = self.testGameBoard.cities["NEWYORK"]
+        montreal = self.testGameBoard.cities["MONTREAL"]
+        chicago = self.testGameBoard.cities["CHICAGO"]
+        london = self.testGameBoard.cities["LONDON"]
+        madrid = self.testGameBoard.cities["MADRID"]
+
+        atlanta.blue = 3
+        washington.blue = 3
+        newyork.blue = 3
+        
+        self.testGameBoard.infectCity("ATLANTA", 'blue')
+        self.assertEquals(london.blue, 1)
+        self.assertEquals(madrid.blue, 1)
+        self.assertEquals(chicago.blue, 1)
+        self.assertEquals(montreal.blue, 2)
+        self.assertEquals(miami.blue, 2)
+        self.assertEquals(newyork.blue, 3)
+        self.assertEquals(atlanta.blue, 3)
+        self.assertEquals(washington.blue, 3)
+
+
 
     def test_processEpidemic(self):
         pass
