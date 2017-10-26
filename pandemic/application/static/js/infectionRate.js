9b60d691b203d884ad269fc671a55c8905ae6ef8
Ben Jourdain <b.jourdain@gmail.com>
2017-10-26 17:07:19 +1300

    GUI Updates
    Movied cities in water back to land
    Moved the top bar around to accomodate up to four players

--- pandemic/application/static/js/infectionRate.js
+++ pandemic/application/static/js/infectionRate.js
@@ -4,7 +4,7 @@ function infectionRate(options) {
 	this.width = options.width;
 	this.height = options.height;
 	this.yPos = options.yPos || 100;
-	this.xPos = options.xPos || 1200;
+	this.xPos = options.xPos || 900;
 	this.radius = options.radius || 25;
 	this.xScale = options.xScale || 1;
 	this.yScale = options.yScale || 1;
@@ -14,24 +14,26 @@ function infectionRate(options) {
 	this.setStage = function(options){
 		this.infectionStage = options.infectionStage;
 	}
-
+    var offSetYPos = 0;
 	this.render = function () {
        for (i =0;i<this.rateArray.length;i++){
-		this.context.beginPath();
-		this.context.arc(this.xPos+(this.radius*2*i), this.yPos, this.radius, 0,Math.PI*2);
-		this.context.fillStyle = 'green';
-		if (i == this.infectionStage){
-			this.context.fillStyle = 'orange';
-		}
+           if(i%2 == 1){offSetYPos = 30;}
+           else{offSetYPos = 0;}
+                this.context.beginPath();
+                this.context.arc(this.xPos+(this.radius*2*(i*0.9)), this.yPos+offSetYPos, this.radius, 0,Math.PI*2);
+                this.context.fillStyle = 'green';
+                if (i == this.infectionStage){
+                    this.context.fillStyle = 'orange';
+                }
 		this.context.fill();
-		canvas.getContext("2d").font="60px Verdana";
+		canvas.getContext("2d").font="55px Verdana";
 		canvas.getContext("2d").fillStyle = 'black';
 		textWidth = canvas.getContext("2d").measureText(i).width;
-		canvas.getContext("2d").fillText(this.rateArray[i],this.xPos+(this.radius*2*i)-(textWidth/2),this.yPos+(this.radius));
+		canvas.getContext("2d").fillText(this.rateArray[i],this.xPos+(this.radius*2*(i*0.9))-(textWidth/2),this.yPos+offSetYPos*0.93+(this.radius));
 	   }
-	canvas.getContext("2d").font="60px Verdana";
+	canvas.getContext("2d").font="45px Verdana";
 	canvas.getContext("2d").fillStyle = 'green';
-	canvas.getContext("2d").fillText("Infection Rate",this.xPos-this.radius,this.yPos-(this.radius*2));
+	canvas.getContext("2d").fillText("Infection Rate",this.xPos-this.radius,this.yPos-(this.radius*1.5));
 	   
 	   
     };
